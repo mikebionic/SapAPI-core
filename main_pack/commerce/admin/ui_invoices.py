bf03c7485d24a7e7eb2b0dd81fc3eb9dc36b5a33
mike <muhammedjepbarov@gmail.com>
2020-07-13 23:51:08 +0500

    res_total should be fixed (not changing)

--- main_pack/commerce/admin/ui_invoices.py
+++ main_pack/commerce/admin/ui_invoices.py
@@ -10,6 +10,8 @@ from sqlalchemy import and_
 from main_pack.base.num2text import num2text,price2text
 import decimal
 from flask import current_app
+from main_pack.models.commerce.models import Res_total
+from main_pack.api.commerce.checkout_utils import totalQtySubstitution
 # invoiceRegNo
 # orderInvRegNo
 # >> search in db for presets
@@ -75,14 +77,53 @@ def ui_order_inv():
 			products = req.get('products')
 			for product in products:
 				try:
+					# check for presence and equality with the editing product
 					thisOInvLine = Order_inv_line.query\
 						.filter(and_(Order_inv_line.GCRecord=='' or Order_inv_line.GCRecord==None),\
 							Order_inv_line.OInvLineId==product['productId']).first()
+					# check the connection of OInvLine to the correct OInv
 					if (thisOInv.OInvId == thisOInvLine.OInvId):
-						thisOInvLine.OInvLinePrice = decimal.Decimal(product['productPrice'])
-						thisOInvLine.OInvLineAmount = product['productQty']
-						thisOInvLine.OInvLineTotal = decimal.Decimal(product['totalPrice'])
-						thisOInvLine.OInvLineFTotal = decimal.Decimal(product['totalPrice'])
+						OInvLineAmount = product['productQty']
+						OInvLinePrice = decimal.Decimal(product['productPrice'])
+						OInvLineTotal = decimal.Decimal(product['totalPrice'])
+
+						## check if new quantity is greater or not
+						# check if the quantity value is positive else exception
+						# if greater - substitude upcoming with present (get the qty of increased)
+						# decrement the resId with result (if config allows the negative values)
+						# if smaller - substitude present with upcoming
+						# increment the resId with result
+						##
+
+						if (OInvLineAmount<=0):
+							OInvLineAmount = 1
+							# # uncomment if I don't want to equal to 1 for errors
+							# print("amount is less than 1: ")
+							# raise Exception
+						res_total = Res_total.query\
+							.filter(and_(Res_total.GCRecord=='' or Res_total.GCRecord==None),\
+								Res_total.ResId==thisOInvLine.ResId).first()
+
+						print(res_total.ResTotBalance)
+						pastQty = thisOInvLine.OInvLineAmount
+						if (pastQty<OInvLineAmount):
+							resulting_changes = OInvLineAmount-pastQty
+							print("substituded greater with result"+str(resulting_changes))
+							totalSubstitutionResult = totalQtySubstitution(res_total.ResTotBalance,resulting_changes)
+							
+							if totalSubstitutionResult['status']==0:
+								raise Exception
+
+							res_total.ResTotBalance = totalSubstitutionResult['totalBalance']
+						elif (pastQty>OInvLineAmount):
+							resulting_changes = pastQty - OInvLineAmount
+							res_total.ResTotBalance += resulting_changes
+
+						thisOInvLine.OInvLinePrice = OInvLinePrice
+						thisOInvLine.OInvLineAmount = OInvLineAmount
+						thisOInvLine.OInvLineTotal = OInvLineTotal
+						thisOInvLine.OInvLineFTotal = OInvLineTotal
+
 					thisOInv.OInvTotal = decimal.Decimal(OInvTotal)
 					thisOInv.OInvFTotal = decimal.Decimal(OInvFTotal)
 					thisOInv.OInvFTotalInWrite = OInvFTotalInWrite
