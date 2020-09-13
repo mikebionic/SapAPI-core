ac505b7a152a6399287f3996fffc0865c9b2676b
mike <muhammedjepbarov@gmail.com>
2020-09-13 21:16:17 +0500

    filtration backend done, need to add filter button in the grid view

--- main_pack/commerce/commerce/v_products.py
+++ main_pack/commerce/commerce/v_products.py
@@ -14,21 +14,57 @@ from flask_login import current_user,login_required
 # Resource and view
 from main_pack.base.invoiceMethods import resource_config_check
 from main_pack.api.commerce.commerce_utils import apiResourceInfo
-from main_pack.models.commerce.models import Resource,Res_total,Barcode,Rating
+from main_pack.models.commerce.models import (Resource,
+																							Res_total,
+																							Barcode,
+																							Rating,
+																							Res_category,
+																							Res_price)
 from main_pack.commerce.commerce.utils import UiCategoriesList,uiSortingData
 from main_pack.api.commerce.commerce_utils import UiCartResourceData
 # / Resource and view /
 
-def collect_resource_paginate_info(pagination_url,page,filtration):
-	pagination_resources = Resource.query\
+
+def collect_resource_paginate_info(pagination_url,
+																	page = 1,
+																	per_page = None,
+																	filtration = None,
+																	category = None):
+	resources = Resource.query\
 		.filter_by(GCRecord = None, UsageStatusId = 1)\
 		.join(Res_total, Res_total.ResId == Resource.ResId)\
 		.filter(and_(
 			Res_total.WhId == 1, 
-			Res_total.ResTotBalance > 0))\
-		.order_by(Resource.CreatedDate.desc())\
-		.paginate(per_page=Config.RESOURCES_PER_PAGE,page=page)
-	
+			Res_total.ResTotBalance > 0))
+	if filtration:
+		if filtration == "date_new":
+			resources = resources.order_by(Resource.CreatedDate.desc())
+		if filtration == "date_old":
+			resources = resources.order_by(Resource.CreatedDate.asc())
+		if filtration == "price_high":
+			resources = resources\
+				.outerjoin(Res_price, Res_price.ResId == Resource.ResId)\
+				.filter(Res_price.ResPriceTypeId == 2 and Res_price.GCRecord == None)\
+				.order_by(Res_price.ResPriceValue.desc())
+		if filtration == "price_low":
+			resources = resources\
+				.outerjoin(Res_price, Res_price.ResId == Resource.ResId)\
+				.filter(Res_price.ResPriceTypeId == 2 and Res_price.GCRecord == None)\
+				.order_by(Res_price.ResPriceValue.asc())
+		if filtration == "rated":
+			resources = resources\
+				.outerjoin(Rating, Rating.ResId == Resource.ResId)\
+				.order_by(Rating.RtRatingValue.asc())
+	if category:
+		category = Res_category.query\
+			.filter_by(GCRecord = None)\
+			.filter(Res_category.ResCatName.ilike(category))\
+			.first()
+		if category:
+			resources = resources.filter(Resource.ResCatId == category.ResCatId)
+
+	pagination_resources = resources.paginate(per_page=per_page if per_page else Config.RESOURCES_PER_PAGE,page=page)
+
 	resource_models = [resource for resource in pagination_resources.items if pagination_resources.items]
 
 	res = apiResourceInfo(resource_models=resource_models)
@@ -40,16 +76,23 @@ def collect_resource_paginate_info(pagination_url,page,filtration):
 		"pages_total": pagination_resources.pages,
 		"next_url": None,
 		"prev_url": None,
-		"filtration":filtration
+		"filtration": filtration,
+		"category": category.ResCatName if category else None
 	}
 	if pagination_resources.has_next:
-		pagination_info["next_url"] = url_for(pagination_url,
-			page=pagination_resources.next_num,
-			filter=filtration)
+		pagination_info["next_url"] = url_for(
+			pagination_url,
+			page = pagination_resources.next_num,
+			per_page = pagination_resources.per_page,
+			filter = pagination_info["filtration"],
+			category = pagination_info["category"])
 	if pagination_resources.has_prev:
-		pagination_info["prev_url"] = url_for(pagination_url,
-			page=pagination_resources.prev_num,
-			filter=filtration)
+		pagination_info["prev_url"] = url_for(
+			pagination_url,
+			page = pagination_resources.prev_num,
+			per_page = pagination_resources.per_page,
+			filter = pagination_info["filtration"],
+			category = pagination_info["category"])
 
 	# method for footer pagination
 	page_num_list = []
@@ -57,7 +100,12 @@ def collect_resource_paginate_info(pagination_url,page,filtration):
 		page_num_info = {
 			"page": page_num,
 			"status": "inactive",
-			"url": url_for(pagination_url,page=page_num,filter=filtration)
+			"url": url_for(
+				pagination_url,
+				page = page_num,
+				per_page = pagination_resources.per_page,
+				filter = pagination_info["filtration"],
+				category = pagination_info["category"])
 		}
 		if page_num == pagination_resources.page:
 			page_num_info["status"] = "active"
@@ -73,10 +121,17 @@ def collect_resource_paginate_info(pagination_url,page,filtration):
 def v_list():
 	page = request.args.get("page",1,type=int)
 	filtration = request.args.get("filter","date",type=str)
+	category = request.args.get("category",None,type=str)
+	per_page = request.args.get("per_page",None,type=int)
 	pagination_url = 'commerce.v_list'
 	categoryData = UiCategoriesList()
 	sortingData = uiSortingData()
-	pagination_info = collect_resource_paginate_info(pagination_url=pagination_url,page=page,filtration=filtration)
+	pagination_info = collect_resource_paginate_info(
+		pagination_url = pagination_url,
+		page = page,
+		per_page = per_page,
+		filtration = filtration,
+		category = category)
 	return render_template(Config.COMMERCE_TEMPLATES_FOLDER_PATH+"commerce/v_list.html",
 		**categoryData,**sortingData,**pagination_info,
 		title=gettext(Config.COMMERCE_LIST_VIEW_TITLE))
@@ -85,10 +140,17 @@ def v_list():
 def v_grid():
 	page = request.args.get("page",1,type=int)
 	filtration = request.args.get("filter","date",type=str)
+	category = request.args.get("category",None,type=str)
+	per_page = request.args.get("per_page",None,type=int)
 	pagination_url = 'commerce.v_grid'
 	categoryData = UiCategoriesList()
 	sortingData = uiSortingData()
-	pagination_info = collect_resource_paginate_info(pagination_url=pagination_url,page=page,filtration=filtration)
+	pagination_info = collect_resource_paginate_info(
+		pagination_url = pagination_url,
+		page = page,
+		per_page = per_page,
+		filtration = filtration,
+		category = category)
 	return render_template(Config.COMMERCE_TEMPLATES_FOLDER_PATH+"commerce/v_grid.html",
 		**categoryData,**sortingData,**pagination_info,
 		title=gettext(Config.COMMERCE_GRID_VIEW_TITLE))
@@ -165,58 +227,3 @@ def resources_grid_search():
 	return render_template(Config.COMMERCE_TEMPLATES_FOLDER_PATH+"commerce/v_search.html",
 		**categoryData,**sortingData,**res,searching_tag=tag,
 		title=gettext(Config.COMMERCE_SEARCH_VIEW_TITLE))
-
-
-# searching and sorting in one demo
-@bp.route(Config.COMMERCE_LIST_VIEW)
-def v_list_search():
-	searching_tag = request.args.get("tag",'',type=str)
-	searching_tag = "%{}%".format(searching_tag)
-	price_low = request.args.get("priceLow",0,type=int)
-	price_high = request.args.get("priceHigh",0,type=int)
-	price_show_type = request.args.get("priceShowType",1,type=int) # 1 = from high to low
-	rating_show_type = request.args.get("ratingShowType",1,type=int) # 1 = from high to low
-	newness_show_type = request.args.get("newness",1,type=int) # 1 = from new to old
-	barcodes = Barcode.query\
-		.filter(and_(
-			Barcode.GCRecord == None,\
-			Barcode.BarcodeVal.ilike(searching_tag)))\
-		.all()
-	
-	resources = Resource.query\
-		.filter(and_(
-			Resource.GCRecord == None,\
-			Resource.ResName.ilike(searching_tag),\
-			Resource.UsageStatusId == 1))\
-		.order_by(Resource.ResId.desc())\
-		.all()
-	resource_list = []
-	if barcodes:
-		for barcode in barcodes:
-			resource_list.append(barcode.ResId)
-	for resource in resources:
-		resource_list.append(resource.ResId)
-
-	if not searching_tag or searching_tag == '':
-		page = request.args.get("page",1,type=int)
-		pagination_resources = Resource.query\
-			.filter_by(GCRecord = None, UsageStatusId = 1)\
-			.join(Res_total, Res_total.ResId == Resource.ResId)\
-			.filter(and_(
-				Res_total.WhId == 1, 
-				Res_total.ResTotBalance > 0))\
-			.order_by(Resource.CreatedDate.desc())\
-			.paginate(per_page=Config.RESOURCES_PER_PAGE,page=page)
-		product_list = []
-		for resource in pagination_resources.items:
-			product = {}
-			product['ResId'] = resource.ResId
-			product_list.append(product)
-		res = apiResourceInfo(product_list)
-
-	categoryData = UiCategoriesList()
-	sortingData = uiSortingData()
-	return render_template(Config.COMMERCE_TEMPLATES_FOLDER_PATH+"commerce/v_list.html",**categoryData,
-		**sortingData,**res,pagination_url='commerce.v_list',
-		pagination_resources=pagination_resources,
-		title=gettext(Config.COMMERCE_SORT_VIEW_TITLE))
