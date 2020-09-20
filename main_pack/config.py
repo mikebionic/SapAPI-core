8da1e8768b06efa9c763857593d4418e156fbf21
mike <muhammedjepbarov@gmail.com>
2020-09-20 11:51:29 +0500

    fixed mail sending issues

--- main_pack/config.py
+++ main_pack/config.py
@@ -58,12 +58,16 @@ class Config:
 	BABEL_DEFAULT_LOCALE = 'tk'
 
 	# MAIL CONFIGURATION
-	MAIL_SERVER = environ.get('MAIL_SERVER')
-	MAIL_PORT = environ.get('MAIL_PORT')
-	MAIL_USE_TLS = environ.get('MAIL_USE_TLS')
+	### testing ##
+	MAIL_SUPPRESS_SEND = False
+	# MAIL_DEBUG = True
+	### / testing /
+	MAIL_SERVER = 'smtp.googlemail.com'
+	MAIL_PORT = 587
+	MAIL_USE_TLS = True
 	MAIL_USERNAME = environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = environ.get('MAIL_PASSWORD')
-
+	
 	# set to True if you want to use BCrypt hashing
 	HASHED_PASSWORDS = False
 
