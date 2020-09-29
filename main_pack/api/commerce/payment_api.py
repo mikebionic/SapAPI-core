030059a06cd03923c9cc1b251a796e0b757a8ead
mike <muhammedjepbarov@gmail.com>
2020-09-29 19:16:15 +0500

    payment type and methods not showing visible index 0

--- main_pack/api/commerce/payment_api.py
+++ main_pack/api/commerce/payment_api.py
@@ -8,11 +8,15 @@ from main_pack.models.commerce.models import Payment_type, Payment_method
 
 @api.route("/tbl-dk-payment-types/")
 def api_paynemt_types():
-	payment_types = Payment_type.query.filter_by(GCRecord = None).all()
+	payment_types = Payment_type.query\
+		.filter_by(GCRecord = None)\
+		.filter(Payment_type.PtVisibleIndex != 0)\
+		.order_by(Payment_type.PtVisibleIndex.asc())\
+		.all()
 	res = {
 		"status": 1,
 		"message": "Payment types",
-		"data": [payment_type.to_json_api() for payment_type in payment_types],
+		"data": [payment_type.to_json_api() for payment_type in payment_types if payment_type.PtVisibleIndex != 0],
 		"total": len(payment_types)
 	}
 	response = make_response(jsonify(res),200)
@@ -21,11 +25,15 @@ def api_paynemt_types():
 
 @api.route("/tbl-dk-payment-methods/")
 def api_payment_methods():
-	payment_methods = Payment_method.query.filter_by(GCRecord = None).all()
+	payment_methods = Payment_method.query\
+		.filter_by(GCRecord = None)\
+		.filter(Payment_method.PmVisibleIndex != 0)\
+		.order_by(Payment_method.PmVisibleIndex.asc())\
+		.all()
 	res = {
 		"status": 1,
 		"message": "Payment methods",
-		"data": [payment_method.to_json_api() for payment_method in payment_methods],
+		"data": [payment_method.to_json_api() for payment_method in payment_methods if payment_method.PmVisibleIndex != 0],
 		"total": len(payment_methods)
 	}
 	response = make_response(jsonify(res),200)
