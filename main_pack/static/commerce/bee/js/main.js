511f66fecf83bb5ee374483fb18f1105d45bae8b
Gulbahar <20shamballa01@gmail.com>
2020-10-09 18:49:07 +0500

    login-da goz dur

--- main_pack/static/commerce/bee/js/main.js
+++ main_pack/static/commerce/bee/js/main.js
@@ -796,3 +796,17 @@ $('body').delegate('.applyFiltersBtn','click',function(){
 //   setTimeout("html5Slider.noUiSlider.set([null, selectMax.value]);", 100);
 
 // });  
+
+var state= false;
+function toggle(){
+    if(state){
+	document.getElementById("password").setAttribute("type","password");
+	document.getElementById("eye").style.color='#7a797e';
+	state = false;
+     }
+     else{
+	document.getElementById("password").setAttribute("type","text");
+	document.getElementById("eye").style.color='#5887ef';
+	state = true;
+     }
+}
