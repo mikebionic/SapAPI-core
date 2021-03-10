5da0a14c71524c84726a16f56907abdd890dce65
Gulbahar <20shamballa01@gmail.com>
2021-03-10 15:00:20 +0500

    update filter, pagination and contact

--- main_pack/static/commerce/ls/js/scripts.js
+++ main_pack/static/commerce/ls/js/scripts.js
@@ -1,3 +1,7 @@
+
+!function(e){e.fn.niceSelect=function(t){function s(t){t.after(e("<div></div>").addClass("nice-select").addClass(t.attr("class")||"").addClass(t.attr("disabled")?"disabled":"").attr("tabindex",t.attr("disabled")?null:"0").html('<span class="current"></span><ul class="list"></ul>'));var s=t.next(),n=t.find("option"),i=t.find("option:selected");s.find(".current").html(i.data("display")||i.text()),n.each(function(t){var n=e(this),i=n.data("display");s.find("ul").append(e("<li></li>").attr("data-value",n.val()).attr("data-display",i||null).addClass("option"+(n.is(":selected")?" selected":"")+(n.is(":disabled")?" disabled":"")).html(n.text()))})}if("string"==typeof t)return"update"==t?this.each(function(){var t=e(this),n=e(this).next(".nice-select"),i=n.hasClass("open");n.length&&(n.remove(),s(t),i&&t.next().trigger("click"))}):"destroy"==t?(this.each(function(){var t=e(this),s=e(this).next(".nice-select");s.length&&(s.remove(),t.css("display",""))}),0==e(".nice-select").length&&e(document).off(".nice_select")):console.log('Method "'+t+'" does not exist.'),this;this.hide(),this.each(function(){var t=e(this);t.next().hasClass("nice-select")||s(t)}),e(document).off(".nice_select"),e(document).on("click.nice_select",".nice-select",function(t){var s=e(this);e(".nice-select").not(s).removeClass("open"),s.toggleClass("open"),s.hasClass("open")?(s.find(".option"),s.find(".focus").removeClass("focus"),s.find(".selected").addClass("focus")):s.focus()}),e(document).on("click.nice_select",function(t){0===e(t.target).closest(".nice-select").length&&e(".nice-select").removeClass("open").find(".option")}),e(document).on("click.nice_select",".nice-select .option:not(.disabled)",function(t){var s=e(this),n=s.closest(".nice-select");n.find(".selected").removeClass("selected"),s.addClass("selected");var i=s.data("display")||s.text();n.find(".current").text(i),n.prev("select").val(s.data("value")).trigger("change")}),e(document).on("keydown.nice_select",".nice-select",function(t){var s=e(this),n=e(s.find(".focus")||s.find(".list .option.selected"));if(32==t.keyCode||13==t.keyCode)return s.hasClass("open")?n.trigger("click"):s.trigger("click"),!1;if(40==t.keyCode){if(s.hasClass("open")){var i=n.nextAll(".option:not(.disabled)").first();i.length>0&&(s.find(".focus").removeClass("focus"),i.addClass("focus"))}else s.trigger("click");return!1}if(38==t.keyCode){if(s.hasClass("open")){var l=n.prevAll(".option:not(.disabled)").first();l.length>0&&(s.find(".focus").removeClass("focus"),l.addClass("focus"))}else s.trigger("click");return!1}if(27==t.keyCode)s.hasClass("open")&&s.trigger("click");else if(9==t.keyCode&&s.hasClass("open"))return!1});var n=document.createElement("a").style;return n.cssText="pointer-events:auto","auto"!==n.pointerEvents&&e("html").addClass("no-csspointerevents"),this}}(jQuery);
+
+
 $(document).ready(function($) {
 	
 	$('.slider-container__tab-item').click(function(){
@@ -61,7 +65,7 @@ $(document).ready(function($) {
 					breakpoint: 1199,
 
 					settings: {
-							slidesToShow: 3,
+							slidesToShow: 4,
 					}
 			},
 			 {
@@ -116,11 +120,11 @@ $(document).ready(function($) {
 			{
 					breakpoint: 991,
 					settings: {
-							slidesToShow: 3,
+							slidesToShow: 4,
 					}
 			},
 			{
-					breakpoint: 767,
+					breakpoint: 800,
 					settings: {
 							autoplay: true,
 							slidesToShow: 3,
@@ -149,6 +153,20 @@ $(document).ready(function($) {
 }); 
 
 
+const button = document.querySelector("#menu-departments-menu ul");
+const done = document.querySelector("#menu-departments-menu ul li");
+console.log(button);
+let added = false;
+button.addEventListener('click', () => {
+  if (added) {
+    done.style.transform = "translate(-110%) skew(-40deg)";
+    added = false;
+  } else
+  {
+    done.style.transform = "translate(0px)";
+    added = true;
+  }
+});
 
 
 
@@ -1003,3 +1021,5 @@ function categoryMenuToggle() {
 slideCategoryByPathname()
 // categoryMenuToggle();
 // $(window).resize(categoryMenuToggle);
+
+$('.nice-select').niceSelect()
