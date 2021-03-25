65b2f727bb6ffff96c83888a44e624634f8f9618
Gulbahar <20shamballa01@gmail.com>
2021-03-25 21:56:23 +0500

    updated category button

--- main_pack/static/commerce/ls/js/scripts.js
+++ main_pack/static/commerce/ls/js/scripts.js
@@ -1628,10 +1628,39 @@ categoryToggle.on('click', function(){
 // });
 
 
-$('.dropdown-toggle').on('click', () =>{
-	$('.cart-overlay').addClass('visible')
+function categoryMenuToggle() {
+	var screenSize = windows.width();
+	if (screenSize <= 799) {
+		$('.dropdown-toggle').on('click', () =>{
+			$('.cart-overlay').addClass('visible')
+		});
+		$('.cart-overlay').on('click', () =>{
+			$('.cart-overlay').removeClass('visible')
+		});
+	}
+	else{
+		slideCategoryByPathname();
+	}
+}
+
+
+
+const imgs = document.querySelectorAll('.img-select a');
+const imgBtns = [...imgs];
+let imgId = 1;
+
+imgBtns.forEach((imgItem) => {
+    imgItem.addEventListener('click', (event) => {
+        event.preventDefault();
+        imgId = imgItem.dataset.id;
+        slideImage();
+    });
 });
 
-$('.cart-overlay').on('click', () =>{
-	$('.cart-overlay').removeClass('visible')
-})
+function slideImage(){
+    const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
+
+    document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+}
+
+window.addEventListener('resize', slideImage);
