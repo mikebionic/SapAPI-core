6a437f5d46eb7dc44cc918bd59b36a1ac90e7a48
mike-bionic <muhammedjepbarov@gmail.com>
2021-05-21 00:34:04 +0500

    some image api corrections

--- main_pack/api/commerce/image_api.py
+++ main_pack/api/commerce/image_api.py
@@ -304,7 +304,7 @@ def api_images():
 				image_req["Image"] = None
 				failed_data.append(image_req)
 			
-			this_image = None
+			thisImage = None
 
 		# db.session.commit()
 		print(f"{datetime.now()} | Images were committed")
