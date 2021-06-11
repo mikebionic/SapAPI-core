9437b2e75e223198a5b63a8f16d12eed86f1084e
mike-bionic <muhammedjepbarov@gmail.com>
2021-06-11 16:41:37 +0500

    updates, removed prints

--- scraper_pack/__init__.py
+++ scraper_pack/__init__.py
@@ -4,6 +4,6 @@ from scraper_pack.parser_config import parser_config
 def main_parser():
 	for config in parser_config:
 		links_list = config["get_links_function"](config["article_links_url"])
-		print(links_list)
+		# print(links_list)
 		configure_link_saving(links_list, config["author"])
-	print("parser done")
+	# print("parser done")
