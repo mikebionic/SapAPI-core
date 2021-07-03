26864dae82776c023eceffe66db5ac15656ea465
mike-bionic <muhammedjepbarov@gmail.com>
2021-07-03 20:09:41 +0500

    updated todos

--- main_pack/base/cryptographyMethods.py
+++ main_pack/base/cryptographyMethods.py
@@ -6,6 +6,10 @@ def encrypt_data(data, server_key, db_guid, client_key):
 		if not data:
 			raise Exception
 
+		# TODO: make server key to use Config.AppWebKey, update RpAccWebKey 
+		# TODO: before inserting to db or any return actions, generate fernet key
+		# and encrypt it with app key with bcrypt or jwt, then save to db
+		# TODO: check that any other generated fernet keys cant decrypt others
 		f = Fernet(server_key)
 		enc = f.encrypt(str(data).encode()).decode()
 		more = f"{str(db_guid)}{enc}{str(client_key)[::-1]}".encode()
@@ -23,6 +27,7 @@ def decrypt_data(data, server_key, db_guid, client_key):
 
 		f = Fernet(server_key)
 		dec = f.decrypt(data.encode()).decode()
+		# TODO: add validator of existence of both guid and client key
 		dec = dec.replace(str(db_guid), '')
 		dec = dec.replace(str(client_key)[::-1], '')
 		dec = f.decrypt(str(dec).encode())
