4015d877fecac2b893adf9712955195db6728ca6
mike-bionic <muhammedjepbarov@gmail.com>
2021-08-01 23:24:01 +0500

    updated errorhandlers

--- main_pack/errors/routes.py
+++ main_pack/errors/routes.py
@@ -9,79 +9,114 @@ from main_pack.commerce.commerce.utils import UiCategoriesList
 
 @bp.app_errorhandler(400)
 def bad_request(error):
+	res = {
+		"error": "Bad request",
+		"message": str(error)
+	}
+
 	if (request.path.startswith(Config.API_URL_PREFIX) or Config.API_AND_ADMIN_ONLY):
-		res = {
-			"error": "Bad request",
-			"message": str(error)
-		}
 		return make_response(jsonify(res), 400)
-	return render_template(f"{Config.COMMERCE_TEMPLATES_FOLDER_PATH}/errors/500.html",**UiCategoriesList()), 500
+
+	return render_template(
+		f"{Config.COMMERCE_TEMPLATES_FOLDER_PATH}/errors/500.html",
+		title = res["error"],
+		**UiCategoriesList()), 500
 
 
 @bp.app_errorhandler(401)
 def unauthorized(error):
+	res = {
+		"error": "Unauthorized",
+		"message": str(error)
+	}
+
 	if (request.path.startswith(Config.API_URL_PREFIX) or Config.API_AND_ADMIN_ONLY):
-		res = {
-			"error": "Unauthorized",
-			"message": str(error)
-		}
 		return make_response(jsonify(res), 401)
-	return render_template(f"{Config.COMMERCE_TEMPLATES_FOLDER_PATH}/errors/500.html",**UiCategoriesList()), 500
+
+	return render_template(
+		f"{Config.COMMERCE_TEMPLATES_FOLDER_PATH}/errors/500.html",
+		title = res["error"],
+		**UiCategoriesList()), 500
 
 
 @bp.app_errorhandler(403)
 def forbidden(error):
+	res = {
+		"error": "Forbidden",
+		"message": str(error)
+	}
+
 	if (request.path.startswith(Config.API_URL_PREFIX) or Config.API_AND_ADMIN_ONLY):
-		res = {
-			"error": "Forbidden",
-			"message": str(error)
-		}
 		return make_response(jsonify(res), 403)
-	return render_template(f"{Config.COMMERCE_TEMPLATES_FOLDER_PATH}/errors/403.html",**UiCategoriesList()), 403
+
+	return render_template(
+		f"{Config.COMMERCE_TEMPLATES_FOLDER_PATH}/errors/403.html",
+		title = res["error"],
+		**UiCategoriesList()), 403
 
 
 @bp.app_errorhandler(404)
 def not_found(error):
+	res = {
+		"error": "Not found",
+		"message": str(error)
+	}
+
 	if (request.path.startswith(Config.API_URL_PREFIX) or Config.API_AND_ADMIN_ONLY):
-		res = {
-			"error": "Not found",
-			"message": str(error)
-		}
 		return make_response(jsonify(res), 404)
-	return render_template(f"{Config.COMMERCE_TEMPLATES_FOLDER_PATH}/errors/404.html",**UiCategoriesList()), 404
+
+	return render_template(
+		f"{Config.COMMERCE_TEMPLATES_FOLDER_PATH}/errors/404.html",
+		title = res["error"],
+		**UiCategoriesList()), 404
 
 
 @bp.app_errorhandler(405)
 def method_not_found(error):
+	res = {
+		"error": "Method not found",
+		"message": str(error)
+	}
+
 	if (request.path.startswith(Config.API_URL_PREFIX) or Config.API_AND_ADMIN_ONLY):
-		res = {
-			"error": "Method not found",
-			"message": str(error)
-		}
 		return make_response(jsonify(res), 405)
-	return render_template(f"{Config.COMMERCE_TEMPLATES_FOLDER_PATH}/errors/405.html",**UiCategoriesList()), 405
+
+	return render_template(
+		f"{Config.COMMERCE_TEMPLATES_FOLDER_PATH}/errors/405.html",
+		title = res["error"],
+		**UiCategoriesList()), 405
 
 
 @bp.app_errorhandler(410)
 def gone(error):
+	res = {
+		"error": "Gone",
+		"message": str(error)
+	}
+
 	if (request.path.startswith(Config.API_URL_PREFIX) or Config.API_AND_ADMIN_ONLY):
-		res = {
-			"error": "Gone",
-			"message": str(error)
-		}
 		return make_response(jsonify(res), 410)
-	return render_template(f"{Config.COMMERCE_TEMPLATES_FOLDER_PATH}/errors/410.html",**UiCategoriesList()), 410
+
+	return render_template(
+		f"{Config.COMMERCE_TEMPLATES_FOLDER_PATH}/errors/410.html",
+		title = res["error"],
+		**UiCategoriesList()), 410
 
 
 @bp.app_errorhandler(500)
 def internal_server_error(error):
+	res = {
+		"error": "Forbidden",
+		"message": str(error)
+	}
+
 	if (request.path.startswith(Config.API_URL_PREFIX) or Config.API_AND_ADMIN_ONLY):
-		res = {
-			"error": "Forbidden",
-			"message": str(error)
-		}
 		return make_response(jsonify(res), 500)
-	return render_template(f"{Config.COMMERCE_TEMPLATES_FOLDER_PATH}/errors/500.html",**UiCategoriesList()), 500
+
+	return render_template(
+		f"{Config.COMMERCE_TEMPLATES_FOLDER_PATH}/errors/500.html",
+		title = res["error"],
+		**UiCategoriesList()), 500
 
 
 @bp.app_errorhandler(CSRFError)
