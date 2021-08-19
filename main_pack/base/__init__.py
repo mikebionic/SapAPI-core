85658d14300d7996f89a89ed1e72083345bccf35
mike-bionic <muhammedjepbarov@gmail.com>
2021-08-19 16:15:49 +0500

    added log_print module

--- main_pack/base/__init__.py
+++ main_pack/base/__init__.py
@@ -5,3 +5,4 @@ bp = Blueprint('base', __name__)
 from main_pack.base import num2text
 from .get_first_from_list import get_first_from_list
 from .get_id_from_list_indexing import get_id_from_list_indexing
+from .log_print import log_print
