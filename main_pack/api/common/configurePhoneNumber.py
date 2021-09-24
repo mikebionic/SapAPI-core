857c1d963669200062bcd05988b6ceb6bcc095a0
mike-bionic <muhammedjepbarov@gmail.com>
2021-09-24 16:42:35 +0500

    added try except in configure phone numebr

--- main_pack/api/common/configurePhoneNumber.py
+++ main_pack/api/common/configurePhoneNumber.py
@@ -1,11 +1,13 @@
 def configurePhoneNumber(number, remove_plus_sign=False):
-	number = number.strip().replace(' ','').replace('-','').replace('(','').replace(')','')
+	number = None
+	try:
+		number = number.strip().replace(' ','').replace('-','').replace('(','').replace(')','')
 
-	if len(number) > 9:
-		number = f"+{number}" if not remove_plus_sign and number[0] != "+" else number
-		number = number[1:] if remove_plus_sign and number[0] == "+" else number
+		if len(number) > 9:
+			number = f"+{number}" if not remove_plus_sign and number[0] != "+" else number
+			number = number[1:] if remove_plus_sign and number[0] == "+" else number
 
-	else:
-		number = None
+	except:
+		pass
 
 	return number
