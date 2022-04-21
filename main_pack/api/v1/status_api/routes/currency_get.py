f7d62deea86422e244807a05efd13dc51167f23a
mike <muhammedjepbarov@gmail.com>
2022-04-21 19:18:17 +0500

    added status api folder

---
+++ main_pack/api/v1/status_api/routes/currency_get.py
@@ -0,0 +1,18 @@
+from flask import request
+from main_pack.api.v1.status_api import api
+from main_pack.api.response_handlers.handle_default_response import handle_default_response
+from main_pack.models import Currency
+
+@api.route("/currency/")
+def currency_get():
+	request_props = {	
+		"id": request.args.get("id", None, type=int),
+		"uuid": request.args.get("uuid", None, type=str),
+		"name": request.args.get("name", None, type=str),
+		"name_tk": request.args.get("name_tk", None, type=str),
+		"name_ru": request.args.get("name_ru", None, type=str),
+		"name_en": request.args.get("name_en", None, type=str),
+	}
+	dbModels = Currency.get_all(**request_props)
+	data = [item.to_json_api() for item in dbModels]
+	return handle_default_response(data, "Currencies")
