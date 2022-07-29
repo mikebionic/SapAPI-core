3b8bb81e6032a8ff86b9aa305d19032f0129f638
merri23 <mekanhodzaberdiev@gmail.com>
2022-07-29 18:27:15 +0500

    done get_invoices with post request

--- main_pack/api/v1/invoice_api/routes/v_invoice.py
+++ main_pack/api/v1/invoice_api/routes/v_invoice.py
@@ -3,22 +3,29 @@ from flask import request, make_response, jsonify
 from datetime import datetime
 
 from main_pack.api.v1.invoice_api import api
-from main_pack.api.auth.utils import token_required
-from main_pack.api.v1.invoice_api.utils import collect_invoice_data
+from main_pack.api.auth.utils import sha_required
+from main_pack.api.v1.invoice_api.utils import get_invoices
 
 
-@api.route("/v-invoices/")
-@token_required
-def v_invoice(user):
-	startDate = request.args.get("startDate",None,type=str)
-	endDate = request.args.get("endDate",datetime.now())
-	current_user = user['current_user']
+@api.route("/v-invoices/", methods=["GET", "POST"])
+@sha_required
+def v_invoice():
+	arg_data = {
+		"DivId": request.args.get("DivId", None, type=int),
+		"notDivId": request.args.get("notDivId", None, type=int),
 
-	res = collect_invoice_data(
-		startDate = startDate,
-		endDate = endDate,
-		rp_acc_user = current_user
-	)
+		"UId": request.args.get("userId", None, type=int),
+		"RpAccId": request.args.get("rpAccId", None, type=int),
+	}
+
+	if request.method == "POST":
+		data = []
+		req = request.get_json()
+		try:
+			arg_data["invoices_to_exclude"] = req if req else None
+		except Exception as ex:
+			print(f"{datetime.now()} | v-invoices by excluded list Exception {ex}")
+		res = get_invoices(**arg_data)
 
 	status_code = 200
 	return make_response(jsonify(res), status_code)
